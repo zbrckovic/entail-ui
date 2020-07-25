7f9ca8b9f6a11729d777827ffb09775539477fd8
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-07-25 21:19:51 +0200

    Bump core version

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,10 @@ import { CommonWrapper } from 'common-wrapper'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { DeductionInterface } from '@zbrckovic/entail-core'
+
+console.log(new DeductionInterface())
+
 ReactDOM.render(
   <CommonWrapper><App/></CommonWrapper>,
   document.getElementById('root')
