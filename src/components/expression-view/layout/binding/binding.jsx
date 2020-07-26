ad075ccfebe7d91e3051f3c25558f96cefce92a8
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-07-26 22:00:54 +0200

    Simplify binding presentation

--- src/components/expression-view/layout/binding/binding.jsx
+++ src/components/expression-view/layout/binding/binding.jsx
@@ -5,10 +5,5 @@ import React, { useContext } from 'react'
 export const Binding = ({ sym }) => {
   const presentationCtx = useContext(SymPresentationCtx)
   const text = presentationCtx.get(sym).getDefaultSyntacticInfo().text
-
-  return <>
-    <ExpressionText text="[" kind={sym.kind}/>
-    <ExpressionText text={text} kind={sym.kind}/>
-    <ExpressionText text="]" kind={sym.kind}/>
-  </>
+  return <ExpressionText text={text} kind={sym.kind}/>
 }
