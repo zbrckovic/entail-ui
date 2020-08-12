fdcf690de18d67e2cc1d3c57d5c3289c56b2d34f
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-08-12 21:16:40 +0200

    Fix styles in ExpressionView storybook

--- src/components/expression-view.stories.jsx
+++ src/components/expression-view.stories.jsx
@@ -52,10 +52,7 @@ export const FullWidthTooLong = () => {
   const { formula, presentationCtx } = useFormula('E[y] A[x] F(x, y) -> A[x] E[y] F(x, y)')
   return (
     <SymPresentationCtx.Provider value={presentationCtx}>
-      <ExpressionView
-        className={'storybook-frame'}
-        expression={formula}
-      />
+      <ExpressionView css={`${exampleMixin}`} expression={formula}/>
     </SymPresentationCtx.Provider>
   )
 }
@@ -65,7 +62,10 @@ const useFormula = formulaText => {
     const parser = new FormulaParser(primitivePresentationCtx)
     const formula = parser.parse(formulaText)
     const presentationCtx = parser.presentationCtx
-    return { formula, presentationCtx }
+    return {
+      formula,
+      presentationCtx
+    }
   })
 
   return state
