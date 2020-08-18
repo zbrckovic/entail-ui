b48a02aa44476df29ea60afa0188233db266c352
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-08-18 18:30:29 +0200

    Styles improvement

--- src/components/expression-text.jsx
+++ src/components/expression-text.jsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/core'
 import { Kind } from '@zbrckovic/entail-core/lib/abstract-structures/sym'
 import React from 'react'
 import { Text } from 'rebass'
@@ -9,11 +8,11 @@ export const ExpressionText = ({ text, kind, ...props }) =>
     variant={kindToVariant[kind] ?? 'eel.neutral'}
     fontFamily='mono'
     fontWeight='semiBold'
-    css={css({
+    sx={{
       display: 'inline-block',
-      whiteSpace: 'pre',
+      whiteSpace: 'inline-block',
       cursor: 'pointer'
-    })}
+    }}
     {...props}
   >
     {text ?? <wbr/>}
