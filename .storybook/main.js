8a854e101bcb92cf633208b097c955545bbf3e90
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-10-08 22:44:47 +0200

    Fix webpack for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -29,7 +29,7 @@ module.exports = {
       test: /\.s[ac]ss$/,
       oneOf: [
         {
-          test: /\.module\.s[ac]ss$/,
+          test: /\.m\.s[ac]ss$/,
           use: [
             development ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
