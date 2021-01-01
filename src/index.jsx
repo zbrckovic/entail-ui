2ef1dee68e77e7405120205379b1149e53bf5728
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2021-01-01 14:48:40 +0100

    Refactoring.

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { RootWrapper } from 'root-wrapper'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
-import { EntryPage } from './pages/entry-page'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { MainPage } from './pages/main-page'
+import { LoginPage } from './pages/entry-page/login-page'
+import { RegisterPage } from './pages/entry-page/register-page'
+import { ForgotPasswordPage } from './pages/entry-page/forgot-password-page'
+import { ChangePasswordPage } from './pages/entry-page/change-password-page'
 
 ReactDOM.render(
   <RootWrapper>
     <BrowserRouter>
       <Switch>
-        <Route path="/">
-          <EntryPage />
+        <Route exact path='/login'>
+          <LoginPage />
         </Route>
-        <Redirect to="/" />
+        <Route exact path='/register'>
+          <RegisterPage />
+        </Route>
+        <Route exact path='/forgot-password'>
+          <ForgotPasswordPage />
+        </Route>
+        <Route exact path='change-password/:token'>
+          <ChangePasswordPage />
+        </Route>
+        <Route path='/'>
+          <MainPage />
+        </Route>
       </Switch>
     </BrowserRouter>
   </RootWrapper>,
---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,107 @@
+import React, { useContext, useEffect } from 'react'
+import { RootCtx } from 'contexts'
+import {
+  Alignment,
+  Button,
+  Intent,
+  Menu,
+  MenuDivider,
+  MenuItem,
+  Navbar,
+  Popover,
+  Position
+} from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom'
+import style from './main-page.m.scss'
+import { useAsyncState } from 'utils/use-async-state'
+import { useTranslation } from 'react-i18next'
+import { UsersPage } from '../users-page'
+import { HomePage } from '../home-page'
+
+export const MainPage = () => {
+  const history = useHistory()
+  const { loggedIn, logout, authenticationService, user } = useContext(RootCtx)
+  const [logoutState, logoutActions] = useAsyncState()
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    if (!logoutState.inProgress) return
+
+    const subscription = authenticationService
+      .logout()
+      .subscribe({
+        complete () {
+          logoutActions.resolve()
+          logout()
+        },
+        error () {
+          logoutActions.reject()
+        }
+      })
+
+    return () => { subscription.unsubscribe() }
+  }, [logoutState.inProgress, authenticationService, logoutActions, logout])
+
+  if (!loggedIn) return <Redirect to='/login' />
+
+  return <div className={style.root}>
+    <Navbar>
+      <Navbar.Group align={Alignment.LEFT}>
+        <Navbar.Heading
+          className={style.headingTitle}
+          onClick={() => { history.push('/') }}
+        >
+          Entail
+        </Navbar.Heading>
+        <Navbar.Divider />
+        <Button minimal icon={IconNames.PROJECTS} text={t('header.projectsLbl')} />
+      </Navbar.Group>
+      <Navbar.Group align={Alignment.RIGHT}>
+        <Popover
+          content={
+            <Menu>
+              <MenuItem
+                text={t('header.logoutLbl')}
+                icon={IconNames.LOG_OUT}
+                onClick={() => { logoutActions.start() }}
+              />
+              {
+                user.isAdmin() && <>
+                  <MenuDivider />
+                  <MenuItem
+                    intent={Intent.DANGER}
+                    text={t('header.usersLbl')}
+                    icon={IconNames.USER}
+                    onClick={() => { history.push('/users') }}
+                  />
+                </>
+              }
+            </Menu>
+          }
+          position={Position.BOTTOM_LEFT}
+        >
+          <Button
+            title={user.email}
+            intent={user.isAdmin() ? Intent.DANGER : Intent.PRIMARY}
+            minimal
+            rightIcon={IconNames.USER}
+          >
+            {user.getUsername().slice(0, 16)}
+          </Button>
+        </Popover>
+      </Navbar.Group>
+    </Navbar>
+    <main>
+      <Switch>
+        <Route path='/users'>
+          <UsersPage />
+        </Route>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </main>
+  </div>
+}
