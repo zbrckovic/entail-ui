76bcc52bb1abd6949157587d762d90469c6cdd22
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2021-06-03 13:11:33 +0200

    Removed storybook

--- .storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import { globalDecorator } from 'storybook/global-decorator'
-
-export const decorators = [globalDecorator]
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-}
--- src/storybook/global-decorator.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { StorybookRootWrapper } from './storybook-root-wrapper'
-
-export const globalDecorator = Story =>
-  <StorybookRootWrapper>
-    <Story />
-  </StorybookRootWrapper>
